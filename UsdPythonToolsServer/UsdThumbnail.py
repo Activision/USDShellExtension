7db464944433f19882e12a73b8155c5516df1ab3
Piotr Mintus <pmintus@activision.com>
2021-06-07 10:31:43 -0700

    Adding copyright headers to python files

--- UsdPythonToolsServer/UsdThumbnail.py
+++ UsdPythonToolsServer/UsdThumbnail.py
@@ -1,3 +1,20 @@
+#  Copyright 2021 Activision Publishing, Inc. 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# This is a fix for USD issue #1521
+# https://github.com/PixarAnimationStudios/USD/issues/1521
+
 import sys
 try:
     from importlib.util import spec_from_loader, module_from_spec
